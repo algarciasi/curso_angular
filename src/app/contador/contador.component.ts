64bcb05fbe266500f3392d42dd1249b3b046f3f1
ALberto Garcia <albertomiguelgarciasimo@outlook.com>
2024-08-06 19:01:32 +0200

    segundo commit

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Persona } from '../persona';
+
 @Component({
   selector: 'app-contador',
   templateUrl: './contador.component.html',
-  styleUrl: './contador.component.css'
+  styleUrl: './contador.component.css',
 })
 export class ContadorComponent {
 
+  persona:Persona = {
+    nombre: 'Alberto Garcia',
+    edad: 30
+  };
 }
